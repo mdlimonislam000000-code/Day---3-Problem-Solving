5a6e25e545b9f48e899c0b0d4695caee9a868d60
Md Limon Mia <mdlimonislam000000@gmail.com>
2026-09-19 22:53:30 +0600

    problem 12 solved

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ const sumArray = (arr) => {
   return arr.reduce((accumulator, current) => accumulator + current, 0);
 };
 console.log('Problem - 11 :',sumArray([1,2,3,4,5,6,7,8,9]))
+
+// Problem 12 
+
+const findMax = (arr) =>{
+    if (arr.length=== 0 ){
+        return null ;
+    }
+
+    let max = arr[0];
+    for ( let i = 1 ; i < arr.length ; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max 
+}
+console.log('Problem - 12 :',findMax([3, 1, 7, 2, 9]));
